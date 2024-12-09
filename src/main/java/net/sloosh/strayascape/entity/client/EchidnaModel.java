16a7b2b21879a58cddfe2eadfe2c9cc4eb5cd0e5
Sloosh <slushietheuwu@gmail.com>
2024-12-09 19:38:10 +1100

    Updated echidna model

--- src/main/java/net/sloosh/strayascape/entity/client/EchidnaModel.java
+++ src/main/java/net/sloosh/strayascape/entity/client/EchidnaModel.java
@@ -1,15 +1,9 @@
 package net.sloosh.strayascape.entity.client;
 
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.MathHelper;
 import net.sloosh.strayascape.Strayascape;
 import net.sloosh.strayascape.entity.custom.EchidnaEntity;
-import software.bernie.geckolib.animation.AnimationProcessor;
-import software.bernie.geckolib.animation.AnimationState;
-import software.bernie.geckolib.cache.object.GeoBone;
-import software.bernie.geckolib.constant.DataTickets;
 import software.bernie.geckolib.model.GeoModel;
-import software.bernie.geckolib.model.data.EntityModelData;
 
 public class EchidnaModel extends GeoModel<EchidnaEntity> {
     @Override
--- src/main/java/net/sloosh/strayascape/entity/client/EchidnaRenderer.java
+++ src/main/java/net/sloosh/strayascape/entity/client/EchidnaRenderer.java
@@ -1,12 +1,9 @@
 package net.sloosh.strayascape.entity.client;
 
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 import net.sloosh.strayascape.Strayascape;
 import net.sloosh.strayascape.entity.custom.EchidnaEntity;
-import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 public class EchidnaRenderer extends GeoEntityRenderer<EchidnaEntity> {
