85b0fb46031ba6cdcec33b6a93f79028b5bac909
Sloosh <slushietheuwu@gmail.com>
2024-12-11 12:05:00 +1100

    fixed mod identifier

--- src/main/java/net/sloosh/strayascape/entity/ModEntities.java
+++ src/main/java/net/sloosh/strayascape/entity/ModEntities.java
@@ -5,13 +5,12 @@ import net.minecraft.entity.SpawnGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.sloosh.strayascape.Strayascape;
 import net.sloosh.strayascape.entity.custom.EchidnaEntity;
 
 public class ModEntities {
     public static final EntityType<EchidnaEntity> ECHIDNA = Registry.register(
             Registries.ENTITY_TYPE,
-            Identifier.of("Strayascape", "echidna"),
-            EntityType.Builder.create(EchidnaEntity::new, SpawnGroup.CREATURE).dimensions(0.75f, 0.75f).build("cube")
+            Identifier.of("strayascape", "echidna"),
+            EntityType.Builder.create(EchidnaEntity::new, SpawnGroup.CREATURE).dimensions(0.75f, 0.75f).build("echidna")
     );
 }
