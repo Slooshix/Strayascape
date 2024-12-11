c292a99accd0760f7350db6697503fdbe1597454
Sloosh <slushietheuwu@gmail.com>
2024-12-11 11:56:41 +1100

    - Lowered GENERIC_MOVEMENT_SPEED and WanderAroundFarGoal - Added idle animation - getAnimatableInstanceCache returns cache instead of null

--- src/main/java/net/sloosh/strayascape/entity/custom/EchidnaEntity.java
+++ src/main/java/net/sloosh/strayascape/entity/custom/EchidnaEntity.java
@@ -20,7 +20,7 @@ import software.bernie.geckolib.animatable.instance.SingletonAnimatableInstanceC
 import software.bernie.geckolib.animation.*;
 
 public class EchidnaEntity extends AnimalEntity implements GeoEntity {
-    private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
+    private final AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
     @Override
     public boolean isBreedingItem(ItemStack stack) {
         return false;
@@ -35,14 +35,14 @@ public class EchidnaEntity extends AnimalEntity implements GeoEntity {
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 16.0D)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 4.0f)
                 .add(EntityAttributes.GENERIC_ATTACK_SPEED, 2.0f)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.3f);
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.2f);
 
     }
 
     @Override
     protected void initGoals() {
         this.goalSelector.add(1, new SwimGoal(this));
-        this.goalSelector.add(2, new WanderAroundFarGoal(this, 0.75f, 1));
+        this.goalSelector.add(2, new WanderAroundFarGoal(this, 0.2f, 1));
         this.goalSelector.add(3, new LookAroundGoal(this));
     }
 
@@ -58,15 +58,15 @@ public class EchidnaEntity extends AnimalEntity implements GeoEntity {
     }
 
     private PlayState predicate(AnimationState<GeoAnimatable> geoAnimatableAnimationState) {
-        if (geoAnimatableAnimationState.isMoving()){
+        if (geoAnimatableAnimationState.isMoving()) {
             geoAnimatableAnimationState.getController().setAnimation(RawAnimation.begin().then("animation.model.walk", Animation.LoopType.LOOP));
         }
-        geoAnimatableAnimationState.getController().setAnimation(RawAnimation.begin().then("animation.model.walk",Animation.LoopType.LOOP));
+        geoAnimatableAnimationState.getController().setAnimation(RawAnimation.begin().then("animation.model.idle",Animation.LoopType.LOOP));
         return PlayState.CONTINUE;
     }
 
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
-        return null;
+        return cache;
     }
 }
